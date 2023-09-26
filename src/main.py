10e3e48b06bab51cd6883e4e524739531c7db41e
Baris Yuksel <baris.yuksel@zivver.com>
2023-09-26 09:50:07 +0200

    code formatting, ci pipeline, dependabot conf

--- src/main.py
+++ src/main.py
@@ -11,20 +11,24 @@ from fastapi.responses import Response
 from pydantic import BaseModel
 
 
-ELASTIC_APM_URL = os.getenv('ELASTIC_APM_URL', 'http://localhost:8200')
+ELASTIC_APM_URL = os.getenv("ELASTIC_APM_URL", "http://localhost:8200")
 
 app = FastAPI()
-apm = make_apm_client({
-    'SERVICE_NAME': 'payment',
-    'SERVER_URL': ELASTIC_APM_URL,
-    'CAPTURE_HEADERS': True,
-    'CAPTURE_BODY': 'all',
-})
+apm = make_apm_client(
+    {
+        "SERVICE_NAME": "payment",
+        "SERVER_URL": ELASTIC_APM_URL,
+        "CAPTURE_HEADERS": True,
+        "CAPTURE_BODY": "all",
+    }
+)
 app.add_middleware(ElasticAPM, client=apm)
 
-@app.get('/_health')
+
+@app.get("/_health")
 async def health_check():
     return Response(status_code=200)
+
 
 class Payment(BaseModel):
     amount: float
@@ -33,15 +37,17 @@ class Payment(BaseModel):
     exp_date: str
     cvv: str
 
+
 class Paid(BaseModel):
     id: str
 
-@app.post('/_private/api/v1/payment')
+
+@app.post("/_private/api/v1/payment")
 async def do_payment(payment: Payment) -> Paid:
     if payment.cvv == "666":
         return Response(status_code=503)
-    
+
     if payment.cvv == "777" and int(random.random() * 100) % 3 == 0:
         return Response(status_code=408)
-    
-    return Paid(id = str(uuid.uuid4()))
+
+    return Paid(id=str(uuid.uuid4()))
